2b8c6aeef20d533f5d906e821d60dd53a117afe3
Sinan Açıkgöz <sinan_acikgoz@outlook.com>
2021-09-11 17:49:32 +0300

    test

--- btcwsac1/btcwsac1/btcwsac1.py
+++ btcwsac1/btcwsac1/btcwsac1.py
@@ -1,1 +1,7 @@
-print(input()+" deneme")
+from lxml import html
+import requests
+
+
+page = requests.get('https://www.nytimes.com/crosswords/game/mini')
+contents = html.fromstring(page.content)
+ACROSS = contents.xpath('//*[@id="root"]/div/div/div[4]/div/main/div[2]/div[2]/article/section[2]/div[1]')
